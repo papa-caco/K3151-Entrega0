bb719cad52df3d3da4ae423a359b031123c944ac
papa_caco <ccomesa@gmail.com>
2019-05-22 19:29:24 -0300

    entrega1

--- src/main/java/dominio/clima/Calor.java
+++
@@ -1,9 +0,0 @@
-package dominio.clima;
-
-public class Calor extends Limites {
-	
-	public Calor(Double temp) {
-		this.tempLimite = temp;
-	}
-
-}
--- src/main/java/dominio/clima/Frio.java
+++
@@ -1,9 +0,0 @@
-package dominio.clima;
-
-public class Frio extends Limites{
-	
-	public Frio(Double temp) {
-			this.tempLimite = temp;
-	}
-	
-}
